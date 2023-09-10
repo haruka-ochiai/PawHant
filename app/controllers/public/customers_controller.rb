f45e0197cefec6e48cb8cffd9e2e7e34e079a33f
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-09-10 06:36:48 +0000

    finish header footer

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,17 +1,30 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice] = "登録情報を変更しました"
+      redirect_to customer_path
+    else
+      render :edit
+    end
   end
 
   def check
   end
 
   def destroy
+  end
+
+  def customer_params
+    params.require(:customer).permit(:name,:postcode, :address, :phone_number, :email)
   end
 
 end
