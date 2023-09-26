79ba45b4ae6fbf0ea8d40bc0aace323e95b94096
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-09-26 11:03:30 +0000

    add_validate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
   has_one :notification, as: :subject, dependent: :destroy
   after_create_commit :create_notifications
 
+  validates :comment, presence: true, length: { minimum: 1 }
+
   private
   def create_notifications
     Notification.create(subject: self, customer: pet_post.customer, action_type: :commented_to_own_post)
