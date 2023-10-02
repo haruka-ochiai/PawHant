ff74f94980f96d26c2c1aa817ab658f10910a92f
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-10-02 07:58:15 +0000

    非同期追加中

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,6 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+
+//= require jquery
+//= require rails-ujs
--- app/controllers/public/sightings_controller.rb
+++ app/controllers/public/sightings_controller.rb
@@ -4,13 +4,11 @@ class Public::SightingsController < ApplicationController
     pet_post = PetPost.find(params[:pet_post_id])
     sighting = current_customer.sightings.new(pet_post_id: pet_post.id)
     sighting.save
-    redirect_to request.referer
   end
 
   def destroy
     pet_post = PetPost.find(params[:pet_post_id])
     sighting = current_customer.sightings.find_by(pet_post_id: pet_post.id)
     sighting.destroy
-    redirect_to request.referer
   end
 end
