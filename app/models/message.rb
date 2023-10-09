42c705908bf4383e21886f16b4b8505ccd294326
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-10-09 11:49:08 +0000

    fix_notification

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,12 @@ class Message < ApplicationRecord
   belongs_to :room
 
   validates :message, presence: true
+
+  has_one :notification, as: :subject, dependent: :destroy
+  after_create_commit :create_notifications
+
+  private
+  def create_notifications
+    Notification.create(subject: self, customer: self.customer, action_type: :received_message)
+  end
 end
