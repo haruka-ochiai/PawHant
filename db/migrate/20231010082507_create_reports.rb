6f92fefe2612033f21bef4502f3ddc89a7a59803
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-10-12 12:48:56 +0000

    finish report

--- db/migrate/20231010082507_create_reports.rb
+++ db/migrate/20231010082507_create_reports.rb
@@ -3,7 +3,7 @@ class CreateReports < ActiveRecord::Migration[6.1]
     create_table :reports do |t|
       t.references :reporter, null: false, foreign_key: { to_table: :customers }
       t.references :reported, null: false, foreign_key: { to_table: :customers }
-      t. :content, null: false, polymorphic: true
+      t.references :content, null: false, polymorphic: true
       t.integer :reason, null: false
       t.integer :report_status, default: 0
 
