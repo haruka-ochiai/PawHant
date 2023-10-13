585043adb52d73cbbcf975f3014bc0f656b7a142
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-10-13 11:19:12 +0000

    fix layout

--- app/models/customer_pet.rb
+++ app/models/customer_pet.rb
@@ -18,7 +18,7 @@ class CustomerPet < ApplicationRecord
   end
 
   # ペットの状況
-  enum pet_status: { normal: 0, lost: 1, found: 2, resolved: 3 }
+  enum pet_status: { normal: 0, lost: 1, found: 2 }
   # ペットの種類
   enum species: { dog: 0, cat: 1, bird: 2, other: 3 }
   enum gender: { male: 0, female: 1, unknown: 2 }
