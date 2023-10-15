8d3142a76b9c0d9c0159b6644c7aa7641b14d5e4
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-10-15 06:36:22 +0000

    finish customer rspec model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -22,8 +22,6 @@ class Customer < ApplicationRecord
   validates :postcode, presence: true, format: { with: /\A\d{7}\z/, message: "はハイフンなしの7桁の数字で入力してください" }, unless: -> { is_guest }
   validates :address, presence: true, unless: -> { is_guest }
   validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "はハイフンなしの10桁または11桁の数字で入力してください" }, unless: -> { is_guest }
-  validates :email, presence: true, uniqueness: true
-
 
   # ログイン権限確認
   def active_for_authentication?
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -27,12 +27,47 @@ describe Customer, type: :model do
       expect(customer).not_to be_valid
       expect(customer.errors[:name]).to include("は既に使用されています")
     end
-    
+
     it "郵便番号のバリデーション" do
     customer = FactoryBot.build(:customer, postcode: "123-456")
     expect(customer).not_to be_valid
     expect(customer.errors[:postcode]).to include("はハイフンなしの7桁の数字で入力してください")
     end
+
+     it "郵便番号のバリデーション" do
+    customer = FactoryBot.build(:customer, postcode: nil)
+    expect(customer).not_to be_valid
+    expect(customer.errors[:postcode]).to include("を入力してください")
+    end
+
+    it "Emailのバリデーション" do
+    should validate_presence_of(:email)
+    should allow_value("valid@email.com").for(:email)
+    should_not allow_value("invalid-email").for(:email)
+    end
+
+    it "住所のバリデーション" do
+    customer = FactoryBot.build(:customer, address: nil)
+    expect(customer).not_to be_valid
+    expect(customer.errors[:address]).to include("を入力してください")
+    end
+
+     it "電話番号のバリデーション" do
+    customer = FactoryBot.build(:customer, phone_number: nil)
+    expect(customer).not_to be_valid
+    expect(customer.errors[:phone_number]).to include("を入力してください")
+    end
+
+    it "電話番号のバリデーション" do
+    customer = FactoryBot.build(:customer,  phone_number: "090-0000-0000")
+    expect(customer).not_to be_valid
+    expect(customer.errors[:phone_number]).to include("はハイフンなしの10桁または11桁の数字で入力してください")
+    end
     
+    it "パスワードが6文字以上であること" do
+    customer = Customer.new(email: "test@example.com", password: "short")
+    expect(customer).not_to be_valid
+    expect(customer.errors[:password]).to include("は6文字以上で入力してください")
+  end
   end
 end
