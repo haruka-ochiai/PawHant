2a7fb60e75c20bda290c3fc453d8bc96d7a0b237
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-10-16 11:50:30 +0000

    finish pet_post

--- spec/system/pet_posts.rb
+++ spec/system/pet_posts.rb
@@ -36,38 +36,69 @@ RSpec.describe 'ログイン後の遷移', type: :system do
   end
   
   it '正常なデータで投稿できることを確認' do
-  customer = create(:customer, email: 'taro@example.com', password: 'password')
-  sign_in customer
-  visit new_pet_post_path
+    customer = create(:customer, email: 'taro@example.com', password: 'password')
+    sign_in customer
+    visit new_pet_post_path
+    
+    @pet_post = FactoryBot.build(:pet_post) 
+     
+    fill_in 'pet_post[age]', with: @pet_post.age
+    choose 'lost_radio'
+    choose 'cat_radio'
+    choose 'male_radio'
+    select '東京都', from: 'pet_post[prefecture]'
+    fill_in 'pet_post[area]', with: @pet_post.area
+    fill_in 'pet_post[occurred_on]', with: @pet_post.occurred_on
+    select '1~5kg', from: 'pet_post[weight]'
+    fill_in 'pet_post[characteristics]', with: @pet_post.characteristics
+    fill_in 'pet_post[description]', with: @pet_post.description
   
-  @pet_post = FactoryBot.build(:pet_post) 
-   
-  fill_in 'pet_post[age]', with: @pet_post.age
-  choose 'lost_radio'
-  choose 'cat_radio'
-  choose 'male_radio'
-  select '東京都', from: 'pet_post[prefecture]'
-  fill_in 'pet_post[area]', with: @pet_post.area
-  fill_in 'pet_post[occurred_on]', with: @pet_post.occurred_on
-  select '1~5kg', from: 'pet_post[weight]'
-  fill_in 'pet_post[characteristics]', with: @pet_post.characteristics
-  fill_in 'pet_post[description]', with: @pet_post.description
-
-  click_button '新規登録'
-
-  # フォームの送信後、適切なリダイレクト先にいることを確認
-  expect(page).to have_current_path(pet_post_path(PetPost.last))
-
-  # データベースにデータが正しく保存されたことを確認
-  expect(PetPost.last.area).to eq('渋谷区')
-  expect(PetPost.last.description).to eq('これはテストです')
+    click_button '新規登録'
   
-  # 内容が表示されているか確認
-  expect(page).to have_content('東京都')
-  expect(page).to have_content('渋谷区')
-  expect(page).to have_content('人懐っこいです')
-  expect(page).to have_content('オス')
-  expect(page).to have_content('猫')
-end
+    # フォームの送信後、適切なリダイレクト先にいることを確認
+    expect(page).to have_current_path(pet_post_path(PetPost.last))
+  
+    # データベースにデータが正しく保存されたことを確認
+    expect(PetPost.last.area).to eq('渋谷区')
+    expect(PetPost.last.description).to eq('これはテストです')
+    
+    # 内容が表示されているか確認
+    expect(page).to have_content('東京都')
+    expect(page).to have_content('渋谷区')
+    expect(page).to have_content('人懐っこいです')
+    expect(page).to have_content('オス')
+    expect(page).to have_content('猫')
+  end
 
+  it '編集後にデータが反映されることを確認' do
+    customer = create(:customer, email: 'taro@example.com', password: 'password')
+    sign_in customer
+  
+    # 編集前のペットポストを作成
+    pet_post = create(:pet_post, customer: customer)
+    visit edit_pet_post_path(pet_post)
+  
+    # 編集フォームに新しいデータを入力
+    new_age = '3歳'
+    new_area = '新しい場所'
+    new_description = 'これは編集後のテストです'
+  
+    fill_in 'pet_post[age]', with: new_age
+    fill_in 'pet_post[area]', with: new_area
+    fill_in 'pet_post[description]', with: new_description
+  
+    click_button '更新する'
+  
+    expect(page).to have_current_path(pet_post_path(pet_post))
+  
+    # データベースにデータが正しく更新されたことを確認
+    pet_post.reload # データベースから最新のデータを取得
+    expect(pet_post.age).to eq(new_age)
+    expect(pet_post.area).to eq(new_area)
+    expect(pet_post.description).to eq(new_description)
+  
+    expect(page).to have_content(new_age)
+    expect(page).to have_content(new_area)
+    expect(page).to have_content(new_description)
+  end
 end
